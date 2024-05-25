ec12204f85079b7fecee7fd9b12c7d269978e230
Mapumba <mapumbantumba@gmail.com>
2024-05-25 20:50:10 +0200

    test

--- src/models/user-response.ts
+++ src/models/user-response.ts
@@ -2,7 +2,6 @@ import { Qualification } from "./qualification";
 import { Subject } from "./subject";
 
 export class UserResponse {
-    // id = 0;
     name = '';
     surname = '';
     username = '';
